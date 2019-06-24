1be0a8c5c23abf36b18b7f7702d3bf77009b3290
Eduardo Jose Silva <eduardo.silva.006@acad.pucrs.br>
2019-06-24 14:55:47 -0300

    Atualizando arquivos

--- read.py
+++ read.py
@@ -1,29 +1,26 @@
-# import load_workbook
 from openpyxl import Workbook
 from openpyxl import load_workbook
 from cogroo_interface import Cogroo
 
-def abrirArquivo(filepath):        
-        wb = load_workbook(filepath)
-        return wb.active
+def abrirArquivo():        
+        return load_workbook(filename = filepath)
         
 def lerPerguntas():
                        
-        for i in range(2, max_row+1):
+        for i in range(1, max_row+1):
                 enunciado = planilha.cell(row = i, column = 2).value
                 classe = planilha.cell(row = i, column = 4).value
               
                 if enunciado != None and classe != None:
-                        pergunta = [i-1, enunciado, classe]       
+                        pergunta = [i, enunciado, classe]       
                         perguntas.append(pergunta)
 
                         if classe not in classes:
                                 classes.add(classe)
-                else:
-                        planilha.delete_cols(i)
-
-        wb = Workbook()
-        wb.save(filename = filepath)                                
+##                else:
+##                        planilha.delete_rows(i)
+##
+##        arquivo.save(filename = "corpus.xlsx")                                
 
 def lematizar(texto):
         return cogroo.lemmatize(texto)
@@ -35,7 +32,9 @@ def extrairTokens(texto):
         return texto.sentences[0].tokens
 
 def classificarPergunta(pergunta):
+
         classe = pergunta[2]
+
         if classe not in dictDePerguntas.keys():
                 perguntasDaClasse = [pergunta]
                 dictDePerguntas[classe] = perguntasDaClasse
@@ -43,45 +42,46 @@ def classificarPergunta(pergunta):
                 perguntasDaClasse = dictDePerguntas[classe]
                 perguntasDaClasse.append(pergunta)
 
-# set file path
-filepath = "C:/Users/Maica/Desktop/T2IA/T2_IA/corpus.xlsx"
-cogroo = Cogroo.Instance()
-
 #variáveis globais
 classes = set()
 perguntas = list()
 dictDePerguntas = dict()
+filepath = "C:/Users/Maica/Desktop/T2IA/T2_IA/corpus.xlsx"
+cogroo = Cogroo.Instance()
 
 #Lendo arquivo
-planilha = abrirArquivo(filepath)
+arquivo = abrirArquivo()
+planilha = arquivo.active
 max_row = planilha.max_row
 max_column = planilha.max_column
 
 #Lendo perguntas
 lerPerguntas()
-classes.discard("Classes")
 
 #Normalizando e separando as perguntas por classes
-##for p in perguntas:
-##        p.append(lematizar(p[1]))
-##        morfo = analisar(p[3])
-##        tokens = extrairTokens(morfo)
-##        p.append(tokens)
-##        classificarPergunta(p)
-##
-##print(perguntas[0])
+for p in perguntas:
+        p.append(lematizar(p[1]))
+        morfo = analisar(p[3])
+        tokens = extrairTokens(morfo)
+        p.append(tokens)
+        classificarPergunta(p)
+
+print (len(classes))
+print (len(dictDePerguntas))
+
+print(perguntas[0])
 
 #classes = list(classes)
 #list.sort(classes)
 #print (classes)
 #print (dictDePerguntas.keys())
 
-##aux = set(dictDePerguntas.keys())
-##print (aux.difference(classes))
-##print (classes.difference(aux))
+aux = set(dictDePerguntas.keys())
+print (aux.difference(classes))
+print (classes.difference(aux))
 ##for c in classes:
 ##        print(c)
-##
+
 ##for c in dictDePerguntas.keys():
 ##        print(c)
 
