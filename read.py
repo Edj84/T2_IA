899508b4db7ec0b5a422890cebf293311438004e
Igor Brehm <igorbrehm@hotmail.com>
2019-06-28 00:06:06 -0300

    full cicle

--- read.py
+++ read.py
@@ -14,10 +14,12 @@ def lerPerguntas():
               
                 if enunciado != None and classe != None:
                         pergunta = [i, enunciado, classe]       
-                        perguntas.append(pergunta)
-
-                        if classe not in classes:
-                                classes.add(classe)                              
+                        if(i%5 == 0):
+                                perguntas_val.append(pergunta)
+                        else:     
+                                perguntas.append(pergunta)
+                                if classe not in classes:
+                                        classes.add(classe)                              
 
 def lematizar(texto):
         return cogroo.lemmatize(texto)
@@ -35,23 +37,25 @@ def extrairTokens(texto):
                         result.append(split[0])
         return result
 
-def classificarPergunta(pergunta):
+def classificarPergunta(pergunta,isTrain):
 
         classe = pergunta[2]
-
-        if classe not in dictDePerguntas.keys():
+        dicionario = dictDePerguntas_val
+        if(isTrain):
+                dicionario = dictDePerguntas
+        if classe not in dicionario.keys():
                 perguntasDaClasse = [pergunta]
-                dictDePerguntas[classe] = perguntasDaClasse
+                dicionario[classe] = perguntasDaClasse
         else:
-                perguntasDaClasse = dictDePerguntas[classe]
+                perguntasDaClasse = dicionario[classe]
                 perguntasDaClasse.append(pergunta)
 
-def criarBoW():
-        for classe in dictDePerguntas: #pego todas as perguntas de cada classe
+def criarBoW(dicionario):
+        for classe in dicionario: #pego todas as perguntas de cada classe
                 tokens = dict() #gero um dicionario dos tokens
-                lista = dictDePerguntas[classe]
-                for i in range (len(lista)):
-                        pergunta = lista[i]
+                listaPerguntas = dicionario[classe]
+                for i in range (len(listaPerguntas)):
+                        pergunta = listaPerguntas[i]
                         pergunta = pergunta[4] #pego os tokens da pergunta atual
                         for token in pergunta:
                                 if token not in tokens.keys():
@@ -65,10 +69,10 @@ def criarBoW():
                                 if(aux not in listaGeral):
                                         listaGeral.append(aux)
                         
-def estruturar():
-        for i in range(len(perguntas)):
+def estruturar(listaPerguntas):
+        for i in range(len(listaPerguntas)):
                 vector = list()
-                aux = perguntas[i]
+                aux = listaPerguntas[i]
                 for j in range(len(aux[4])):
                         palavra = aux[4][j]
                         for k in range(len(listaGeral)):
@@ -79,26 +83,46 @@ def estruturar():
                         result.append(0)
                 for index in vector:
                         result[index] = 1
-                perguntas[i].append(result)
+                listaPerguntas[i].append(result)
 
-def criarOutput():
-        f= open(outpath,"w+")
+def criarOutput(lista,isTrain):
+        path = outpath
+        if(isTrain == False):
+                path = os.getcwd()+"/weka_val.arff"
+        f= open(path,"w+")
         f.write("@relation Arquivo\n\n")
         for i in range(len(listaGeral)):
-                f.write("@attribute P"+str(i) +" numeric"+"\n")
+                f.write("@attribute "+ listaGeral[i] +" numeric"+"\n")
         aux = str(classes)
         aux = aux.strip('[]')
         f.write("@attribute classe "+aux+"\n")
         f.write("\n@data\n")
-        for i in range(len(perguntas)):
-                aux = str(perguntas[i][5])
-                aux = aux.strip('[]')
-                f.write(aux+", "+perguntas[i][2] +"\n")
+        if(isTrain):
+                for i in range(len(lista)):
+                        aux = str(lista[i][5])
+                        aux = aux.strip('[]')
+                        f.write(aux+", "+lista[i][2] +"\n")
+        else:
+                for i in range(len(lista)):
+                        aux = str(lista[i][5])
+                        aux = aux.strip('[]')
+                        f.write(aux+", ?\n")
         f.close()
+
+def processarPalavras(lista,isTrain):
+        for p in lista:
+                p.append(lematizar(p[1]))
+                morfo = analisar(p[3])
+                tokens = extrairTokens(morfo)
+                p.append(tokens)
+                classificarPergunta(p,isTrain)
+
 #variáveis globais
 classes = set() #lista de todas as classes existentes
 perguntas = list() #lista com todas as perguntas, suas infos e tokens relevantes
+perguntas_val = list() #lista com todas as perguntas que foram separadas para validacao
 dictDePerguntas = dict() #dicionario com as chaves sendo as classes, os dados as perguntas
+dictDePerguntas_val = dict()
 listaGeral = list() #lista geral dos termos que mais aparecem, BoW
 
 filepath = os.getcwd()+"/corpus.xlsx"
@@ -119,22 +143,15 @@ max_column = planilha.max_column
 lerPerguntas()
 
 #Normalizando e separando as perguntas por classes
-for p in perguntas:
-        p.append(lematizar(p[1]))
-        morfo = analisar(p[3])
-        tokens = extrairTokens(morfo)
-        p.append(tokens)
-        classificarPergunta(p)
+processarPalavras(perguntas,True)
+processarPalavras(perguntas_val,False)
 
-criarBoW()
+criarBoW(dictDePerguntas)
 
-list = listaGeral.sort()
+estruturar(perguntas)
+estruturar(perguntas_val)
 
-for p in list:
-        print(p)
-
-estruturar()
-
-criarOutput()
+criarOutput(perguntas,True)
+criarOutput(perguntas_val,False)
 
 print("DONE")
